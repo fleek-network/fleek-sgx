4c9c41c936c70aaf0ea027c9858bb97985149ddc
daltoncoder <daltoncoder2@gmail.com>
2024-08-22 19:30:51 -0400

    steps 1+2 untested

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 pub mod test_tls;
 pub mod types;
 
-use types::{Quote, SgxCollateral};
+use anyhow::{bail, Context};
+use mbedtls::alloc::List;
+use mbedtls::x509::{Certificate, Crl};
+use types::{get_intel_pub_key, Quote, SgxCollateral};
+use x509_parser::prelude::{parse_x509_pem, CertificateRevocationList, FromDer};
 
-pub fn verify_remote_attestation(_quote: Quote, _collateral: SgxCollateral) -> anyhow::Result<()> {
+pub fn verify_remote_attestation(quote: Quote, collateral: SgxCollateral) -> anyhow::Result<()> {
     // 1. Verify the integrity of the signature chain from the Quote to the Intel-issued PCK
     //    certificate.
-    verify_integrity()?;
-
-    // 2. Verify no keys in the chain have been revoked.
-    verify_revocation()?;
+    // 2. Verify no keys in the chain have been revoked.s
+    verify_integrity(collateral, quote)?;
 
     // 3. Verify the Quoting Enclave is from a suitable source and is up to date.
     verify_quote()?;
@@ -23,12 +25,102 @@ pub fn verify_remote_attestation(_quote: Quote, _collateral: SgxCollateral) -> a
     Ok(())
 }
 
-fn verify_integrity(/* ...*/) -> anyhow::Result<()> {
-    todo!()
+fn verify_integrity(collateral: SgxCollateral, _quote: Quote) -> anyhow::Result<()> {
+    let tcb_issuer_chain =
+        Certificate::from_pem_multiple(collateral.tcb_info_issuer_chain.as_bytes())?;
+
+    let (_, root_crl_pem) = parse_x509_pem(collateral.root_ca_crl.as_bytes())?;
+    let root_crl_der = root_crl_pem.contents;
+
+    let root_ca = tcb_issuer_chain
+        .iter()
+        .last()
+        .context("Tcb issuer chain is empty")?;
+
+    verify_root_ca(root_ca, &root_crl_der)?;
+
+    // Now that we verified signed the root certificate we and crl list we can trust them
+    let mut trusted_crl = Crl::new();
+    trusted_crl.push_from_der(&root_crl_der)?;
+    trusted_crl.push_from_pem(collateral.pck_crl.as_bytes())?;
+
+    let mut trusted_root_ca = List::<Certificate>::new();
+    trusted_root_ca.push(root_ca.clone());
+
+    let pck_issuer_crl_chain =
+        Certificate::from_pem_multiple(collateral.pck_crl_issuer_chain.as_bytes())?;
+
+    // todo maybe use this err_info to print better error here
+    Certificate::verify(
+        &pck_issuer_crl_chain,
+        &trusted_root_ca,
+        Some(&mut trusted_crl),
+        None,
+    )?;
+
+    Certificate::verify(
+        &tcb_issuer_chain,
+        &trusted_root_ca,
+        Some(&mut trusted_crl),
+        None,
+    )?;
+
+    let pck_cert_chain = Certificate::from_pem_multiple(collateral.pck_signing_chain.as_bytes())?;
+
+    Certificate::verify(
+        &pck_cert_chain,
+        &trusted_root_ca,
+        Some(&mut trusted_crl),
+        None,
+    )?;
+
+    let qe_id_issuer_chain =
+        Certificate::from_pem_multiple(collateral.qe_identity_issuer_chain.as_bytes())?;
+
+    Certificate::verify(
+        &qe_id_issuer_chain,
+        &trusted_root_ca,
+        Some(&mut trusted_crl),
+        None,
+    )?;
+
+    Ok(())
 }
 
-fn verify_revocation(/* ...*/) -> anyhow::Result<()> {
-    todo!()
+fn verify_root_ca(root_ca: &Certificate, root_crl_der: &[u8]) -> anyhow::Result<()> {
+    // We need to verify the root certificate is self signed
+    if root_ca.issuer()? != root_ca.subject()? {
+        bail!("Root cert authority is not self signed");
+    }
+
+    // We need to verify that the Trusted Root intel key has signed the certificate
+    let sig = root_ca.signature()?;
+
+    let mut bytes = [0; 64];
+    let hash_len = mbedtls::hash::Md::hash(root_ca.digest_type(), root_ca.as_der(), &mut bytes)?;
+    let hash = &bytes[0..hash_len];
+
+    let mut intel_pub_key = get_intel_pub_key();
+
+    intel_pub_key.verify(root_ca.digest_type(), hash, &sig)?;
+
+    // Verify that the CRL is signed by Intel
+    let (_, root_crl) = CertificateRevocationList::from_der(root_crl_der)?;
+
+    // todo: Should we check the signature/hashing algorithm or is it safe to assume SECP256R1 with
+    // sha256?
+
+    let crl_sig = root_crl.signature_value.as_ref();
+
+    let mut bytes = [0; 64];
+    let hash_len = mbedtls::hash::Md::hash(mbedtls::hash::Type::Sha256, root_crl_der, &mut bytes)?;
+    let hash = &bytes[0..hash_len];
+
+    intel_pub_key.verify(mbedtls::hash::Type::Sha256, hash, crl_sig)?;
+
+    // Now we can trust the root ca and the root crl
+
+    Ok(())
 }
 
 fn verify_quote(/* ...*/) -> anyhow::Result<()> {
--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,60 @@
+use std::str::FromStr;
+
+use mbedtls::bignum::Mpi;
+use mbedtls::ecp::EcPoint;
+use mbedtls::pk::{EcGroup, Pk};
 use serde::{Deserialize, Serialize};
+
+lazy_static::lazy_static! {
+    pub static ref INTEL_PKEY: Pk = {
+        let ec_group = get_prime256_v1_ec_group();
+
+        let point = EcPoint::from_binary(&ec_group, INTEL_ROOT_PUB_KEY).unwrap();
+
+        Pk::public_from_ec_components(ec_group, point).unwrap()
+    };
+}
+
+pub const SECP256R1_OID_STRING: &str = "1.2.840.10045.3.1.7";
+
+/// Intel public key that signs all root certificates for DCAP
+const INTEL_ROOT_PUB_KEY: &[u8] = &[
+    0x04, 0x0b, 0xa9, 0xc4, 0xc0, 0xc0, 0xc8, 0x61, 0x93, 0xa3, 0xfe, 0x23, 0xd6, 0xb0, 0x2c, 0xda,
+    0x10, 0xa8, 0xbb, 0xd4, 0xe8, 0x8e, 0x48, 0xb4, 0x45, 0x85, 0x61, 0xa3, 0x6e, 0x70, 0x55, 0x25,
+    0xf5, 0x67, 0x91, 0x8e, 0x2e, 0xdc, 0x88, 0xe4, 0x0d, 0x86, 0x0b, 0xd0, 0xcc, 0x4e, 0xe2, 0x6a,
+    0xac, 0xc9, 0x88, 0xe5, 0x05, 0xa9, 0x53, 0x55, 0x8c, 0x45, 0x3f, 0x6b, 0x09, 0x04, 0xae, 0x73,
+    0x94,
+];
+
+pub fn get_intel_pub_key() -> Pk {
+    let ec_group = get_prime256_v1_ec_group();
+
+    let point = EcPoint::from_binary(&ec_group, INTEL_ROOT_PUB_KEY).unwrap();
+
+    Pk::public_from_ec_components(ec_group, point).unwrap()
+}
+
+/// Curve used on x509 signatures
+/// todo: we should find a way to make this const
+pub fn get_prime256_v1_ec_group() -> EcGroup {
+    let p = Mpi::from_str("0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff")
+        .unwrap();
+    let a = Mpi::from_str("0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc")
+        .unwrap();
+
+    let b = Mpi::from_str("0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b")
+        .unwrap();
+
+    let g_x = Mpi::from_str("0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296")
+        .unwrap();
+    let g_y = Mpi::from_str("0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5")
+        .unwrap();
+
+    let order = Mpi::from_str("0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551")
+        .unwrap();
+
+    EcGroup::from_parameters(p, a, b, g_x, g_y, order).unwrap()
+}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SgxCollateral {
