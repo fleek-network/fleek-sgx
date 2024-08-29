300cd923804b204ca29bc29072ccb61c2a969044
ozwaldorf <self@ossian.dev>
2024-08-29 14:14:49 -0400

    fix: minor amends

--- flake.nix
+++ flake.nix
@@ -49,7 +49,9 @@
           # Common arguments
           commonArgs = {
             inherit src;
-            packages = [ pkgs.protobuf ];
+            strictDeps = true;
+            pname = "fleek-remote-attestation";
+            version = "0.1.0";
             nativeBuildDeps = with pkgs; [ pkg-config ];
             buildInputs = with pkgs; [
               cacert
@@ -60,15 +62,13 @@
               # For linking to `dcap_quoteprov`
               sgx-azure-dcap-client
             ];
-            strictDeps = true;
-            pname = "fleek-remote-attestation";
-            version = "0.1.0";
           } // commonVars;
 
           commonVars = {
             OPENSSL_NO_VENDOR = 1;
             OPENSSL_LIB_DIR = "${pkgs.lib.getLib pkgs.openssl_3}/lib";
             OPENSSL_INCLUDE_DIR = "${pkgs.lib.getDev pkgs.openssl_3.dev}/include";
+
           };
 
           # Build *just* the cargo dependencies, so we can reuse all of that
@@ -107,15 +107,44 @@
             );
           };
 
+          packages = rec {
+            default = fn-service-3;
+
+            fn-service-3 = craneLib.buildPackage (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                pname = "fn-service-3";
+                doCheck = false;
+                cargoExtraArgs = "--locked --bin fn-service-3";
+                FN_ENCLAVE_BIN_PATH = "${fn-sgx-enclave}/bin/fleek-service-sgx-enclave";
+              }
+            );
+
+            # TODO: patch elf into sgxs beforehand
+            fn-sgx-enclave = craneLib.buildPackage {
+              inherit src;
+              cargoArtifacts = null;
+              doCheck = false;
+              cargoToml = "${src}/service/enclave/Cargo.toml";
+              cargoLock = "${src}/service/enclave/Cargo.lock";
+              postUnpack = ''
+                cd $sourceRoot/service/enclave
+                sourceRoot="."
+              '';
+              pname = "fn-sgx-enclave";
+            };
+          };
+
           # Allow using `nix develop` on the project
           devShells.default = craneLib.devShell (
-            {
+            commonVars
+            // {
               # Inherit inputs from checks
               checks = self.checks.${system};
               name = "fleek-sgx-dev";
               packages = with pkgs; [ rust-analyzer ];
             }
-            // commonVars
           );
 
           # Allow using `nix fmt` on the project
