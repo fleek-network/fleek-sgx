25406a7df1f27e50b6acde9e084654fdde00b147
Matthias Wright <matthias.s.wright@gmail.com>
2024-08-29 21:17:31 -0400

    feat(ra-tls): client connection dial

---
+++ lib/ra-tls/src/client.rs
@@ -0,0 +1,50 @@
+use std::io::Write;
+use std::net::TcpStream;
+use std::sync::Arc;
+
+use anyhow::{Context, Result};
+use fleek_remote_attestation::types::report::MREnclave;
+use rustls::pki_types::{CertificateDer, IpAddr, PrivateKeyDer, PrivatePkcs1KeyDer, ServerName};
+use rustls::{ClientConnection, StreamOwned, SupportedCipherSuite};
+
+use crate::cert::{Certificate, PrivateKey};
+use crate::verifier::RemoteAttestationVerifier;
+
+#[allow(unused)]
+fn connect(
+    mr_enclave: MREnclave,
+    server_ip: String,
+    server_port: u16,
+    key: PrivateKey,
+    cert: Certificate,
+) -> Result<(
+    StreamOwned<ClientConnection, TcpStream>,
+    SupportedCipherSuite,
+)> {
+    let private_key = PrivatePkcs1KeyDer::from(key);
+    let private_key = PrivateKeyDer::from(private_key);
+    let cert = CertificateDer::from(cert);
+    let mut config =
+        rustls::ClientConfig::builder_with_provider(Arc::new(rustls_rustcrypto::provider()))
+            .with_safe_default_protocol_versions()?
+            .dangerous()
+            .with_custom_certificate_verifier(Arc::new(RemoteAttestationVerifier::new(mr_enclave)))
+            .with_client_auth_cert(vec![cert], private_key)?;
+
+    config.key_log = Arc::new(rustls::KeyLogFile::new());
+
+    let server_name = ServerName::IpAddress(
+        IpAddr::try_from(server_ip.as_ref()).context("Failed to parse IP address")?,
+    );
+    let conn = ClientConnection::new(Arc::new(config), server_name)?;
+    let sock = TcpStream::connect(format!("{server_ip}:{server_port}"))?;
+    let mut tls = StreamOwned::new(conn, sock);
+
+    tls.write_all("hello".as_bytes())?;
+    let ciphersuite = tls
+        .conn
+        .negotiated_cipher_suite()
+        .context("Failed to negotiate cipher suite failed")?;
+
+    Ok((tls, ciphersuite))
+}
