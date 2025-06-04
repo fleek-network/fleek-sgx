3d68344390b087708f67edaaa9af7fb28bd06bc5
ozwaldorf <self@ossian.dev>
2025-06-04 16:48:18 -0400

    chore: cargo fmt

--- enclave/src/main.rs
+++ enclave/src/main.rs
@@ -29,7 +29,10 @@ fn main() -> Result<(), EnclaveError> {
 
     // Start mutual TLS server for communication with the enclaves on the other nodes
     let our_mrenclave = report.mrenclave;
-    println!("Finished initialization for MRENCLAVE: {}", hex::encode(our_mrenclave));
+    println!(
+        "Finished initialization for MRENCLAVE: {}",
+        hex::encode(our_mrenclave)
+    );
 
     let server_config = build_config_mtls(
         tls_secret_key.clone(),
